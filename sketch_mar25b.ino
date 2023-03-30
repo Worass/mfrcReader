1b4960bc108ba3ae911b9769451bc8ae6b35bb7f
Woras <79487401+Worass@users.noreply.github.com>
2023-03-30 04:11:31 +0300

    Update sketch_mar25b.ino

--- sketch_mar25b.ino
+++ sketch_mar25b.ino
@@ -6,74 +6,67 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
-struct ScannedTag {
-  String tagID;
-  int count;
-};
-
-ScannedTag scannedTags[10]; // Array to store scanned tags
-int numScannedTags = 0; // Number of scanned tags
-
-bool hasRepeatingChars(String tagID) {
-  int i = 0;
-  int len = tagID.length();
-  while (i < len - 2) {
-    if (tagID[i] == tagID[i+1] && tagID[i] == tagID[i+2]) {
-      return true;
-    }
-    i++;
-  }
-  return false;
-}
-
 void setup() {
   Serial.begin(9600);   // Initialize serial communications
+  while (!Serial);      // Wait for serial port to connect
   SPI.begin();          // Initialize SPI bus
   mfrc522.PCD_Init();   // Initialize MFRC522
 }
 
-String getTagID(byte *buffer, byte bufferSize) {
-  String tagID = "";
-  for (byte i = 0; i < bufferSize; i++) {
-    tagID += String(buffer[i] < 0x10 ? "0" : "");
-      tagID += String(buffer[i], HEX);
+void dumpInfoToSerial(MFRC522::Uid uid) {
+  Serial.print("Card UID: ");
+  for (byte i = 0; i < 4; i++) {
+    Serial.print(uid.uidByte[i] < 0x10 ? "0" : "");
+    Serial.print(uid.uidByte[i], HEX);
   }
-  return tagID;
+  Serial.println();
+  
+  byte manufacturer = uid.uidByte[0];
+  String manufacturerName;
+  switch(manufacturer) {
+    case 0x04:
+      manufacturerName = "NXP Semiconductors";
+      break;
+    default:
+      manufacturerName = "Unknown";
+  }
+  Serial.print("Manufacturer: ");
+  Serial.println(manufacturerName);
+
+  byte chipType = uid.uidByte[1];
+  String chipTypeName;
+  switch(chipType) {
+    case 0x08:
+      chipTypeName = "MIFARE Classic 1K";
+      break;
+    case 0x18:
+      chipTypeName = "MIFARE Classic 4K";
+      break;
+    default:
+      chipTypeName = "Unknown";
+  }
+  Serial.print("Chip Type: ");
+  Serial.println(chipTypeName);
+
+  Serial.print("Unique Serial Number: ");
+  for (byte i = 2; i < 4; i++) {
+    Serial.print(uid.uidByte[i] < 0x10 ? "0" : "");
+    Serial.print(uid.uidByte[i], HEX);
+  }
+  Serial.println();
 }
 
 void loop() {
   // Look for new cards
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    // Get tag ID
-    String tagID = getTagID(mfrc522.uid.uidByte, mfrc522.uid.size);
-    // Check if tag ID has been scanned before
-    bool isNewTag = true;
-    for (int i = 0; i < numScannedTags; i++) {
-      if (tagID == scannedTags[i].tagID) {
-        isNewTag = false;
-        scannedTags[i].count++;
-        if (scannedTags[i].count > 1) {
-          Serial.print("Repeated tag: ");
-          Serial.print(scannedTags[i].tagID);
-          Serial.print(", scanned ");
-          Serial.print(scannedTags[i].count);
-          Serial.println(" times.");
-        }
-        break;
-      }
-    }
-    if (isNewTag) {
-      if (!hasRepeatingChars(tagID)) {
-        scannedTags[numScannedTags] = {tagID, 1};
-        numScannedTags++;
-        Serial.print("New tag: ");
-        Serial.println(tagID);
-      } else {
-        Serial.println("Tag has repeating digits or letters.");
-      }
-    } else {
-      Serial.println("Tag has been scanned before.");
-    }
-    mfrc522.PICC_HaltA(); // Halt PICC
+    // Print a dividing line between scanned cards
+    Serial.println("--------------------");
+
+    // Dump detailed information about the card
+    dumpInfoToSerial(mfrc522.uid);
+
+    // Halt PICC and stop encryption
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
   }
 }
